b12587986ba2482e46486c914fc2dc531e5df81d
EstebanChvz <estebanchvzal@gmail.com>
2025-05-14 10:59:29 -0600

    Empezar_la_intro
    Primeros parrafos.

--- P3_POO/P3_POO.cpp
+++ P3_POO/P3_POO.cpp
@@ -1,10 +1,25 @@
-// P3_POO.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-
+// Esteban Chavez Alvarez
+// P3_POO
+//
 
 #include <iostream>
+#include <string>  
+#include <conio.h>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::string nombre;
+    std::cout << "Te damos la bienvenida a (Nombre del juego)\n";
+    std::cout << "Por favor introduzca el nombre de su personaje: \n";
+    std::cin >> nombre;
+    std::cout << "Perfecto!!!" << nombre << "\nQue nombre mas epico!!!\n";
+    std::cout << "Muy bien ahora si podemos empezar\n";
+    std::cout << "Los acontecimientos de esta historia tienen lugar en una ciudad como cualquier otra, en un dia como cualquier otro, en el año 2058.\n";
+    std::cout << "Lastimosamente se puede decir que no han habido muchos cambios en cuanto a tecnologia, ni existen coches voladores como soliamos pensar. Si es triste.\n";
+    std::cout << "Bueno, aqui nos encontramos contigo " << nombre << ". Te encuentras en tu automovil, en medio del trafico. Acabas de regresar de una tranquila visita al sementerio\n";
+    std::cout << "Ya que como cada anio, vas a dejarle flores a tu siempre amada pareja y a platicar con ella de como te esta yendo en la vida.\n";
+    std::cout << "Y aunque ya no estas en duelo, es una actividad que sueles para no olvidar a esa persona tan amada.\n";
+    std::cout << "El trafico esta muy pesado, y quieres ver por que no avanza, asi que te bajas de tu auto a investigaa.\n";
+    std::cout << "Entonces encuentras el problema."
 }
 
